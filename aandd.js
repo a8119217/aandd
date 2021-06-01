198ea1d30bae4185e24138b04b32d0b1c2ee9739
DESKTOP-L9PB5TF\yone_ <a8119217@gsuite.si.aoyama.ac.jp>
2021-06-01 20:34:07 +0900

    pra4-4

--- aandd.js
+++ aandd.js
@@ -1,4 +1,4 @@
-function show(array) {
+/*function show(array) {
     var html = '';
     html = html + '<table border="1">';
     html = html + '<tr>';
@@ -9,9 +9,43 @@ function show(array) {
     html = html + '</table>';
     //document.getElementById('res').innerHTML = html;
     document.getElementById('res').insertAdjacentHTML('beforeend', html);
-}
+}*/
 function createSequentialNumbers(array, n){
     for(var i=1;i<n;i++){
        array.push(i);
+    }
+}
+
+//この関数を，aaandd.jsに加えてください．
+function createOrderedRandomNumbers(numbers, n) {
+    var count = 0;
+    for (var i = 1; ; i++) {
+        if (Math.floor(Math.random() * 3) == 0) {
+            numbers.push(i);
+            count++;
+            if (count >= n) {
+                break;
+            }
+        }
+    }
+}
+
+//配列以外の変数を表示するために，aaandd.jsの中身を新しいshowに置き換えてください．
+function show(obj) {
+    if (Array.isArray(obj)) {
+        var html = '';
+        html = html + '<table border="1">';
+        html = html + '<tr>';
+        for (var i = 0; i < obj.length; i++) {
+            html = html + '<td>' + obj[i] + '</td>';
+        }
+        html = html + '</tr>';
+        html = html + '</table>';
+        //html = html + '<br>';
+        document.getElementById('res').insertAdjacentHTML('beforeend', html);
+    } else {
+        var string = JSON.stringify(obj);
+        string = string + '<br>';
+        document.getElementById('res').insertAdjacentHTML('beforeend', string);
     }
 }
